8edaa184f78a2adffbc59f35ac940cb2f4e14bcb
idror67 <idror1967@gmail.com>
2025-01-11 09:19:54 -0500

    first commit

---
+++ webhook.py
@@ -0,0 +1,26 @@
+import json
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        payload = request.json
+        repository_name = payload['repository']['name']
+        commit_id = payload['head_commit']['id']
+        changed_files = [file['filename'] for file in payload['head_commit']['modified']]
+
+        data = {
+            "repository_name": repository_name,
+            "commit_id": commit_id,
+            "changed_files": changed_files
+        }
+
+        with open('webhook_output.json', 'w') as json_file:
+            json.dump(data, json_file, indent=4)
+
+        return 'Webhook received', 200
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
