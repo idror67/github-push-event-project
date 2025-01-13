76b85716f0d6273d6b648ed428fb488bd6c072ba
idror67 <144365018+idror67@users.noreply.github.com>
2025-01-13 23:48:53 +0200

    Update main.tf
    update3 main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,18 @@
 terraform {
   required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~>6.4.0"
+    }
     linode = {
       source  = "linode/linode"
-      version = "2.25.0"
+      version = "~>2.25.0"
     }
   }
+}
+
+provider "github" {
+  token = var.github_token
 }
 
 provider "linode" {
